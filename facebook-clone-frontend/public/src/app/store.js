9d0d4b9ecbd01c742a99e3113ca7b51bc207e743
Taro Serigano <taroserigano@gmail.com>
2022-04-20 08:50:37 -0500

    Delete facebook-clone-frontend directory

--- facebook-clone-frontend/public/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import postReducer from "../features/postSlice";
-
-export default configureStore({
-  reducer: {
-    post: postReducer,
-  },
-});
